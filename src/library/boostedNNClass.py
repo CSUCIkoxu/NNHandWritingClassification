1c59685dc5f17acffd74932ea67b7a89b369c99b
CSUCIkoxu <70290605+CSUCIkoxu@users.noreply.github.com>
2022-07-18 13:35:25 -0700

    Initialized scripts for the boostedCNN
    Created the boosted CNN script to run a CNN with a boosted architecture.
    
    Created the boostedNNClass to allow the creation of boosted NN architecture with multiple models working in series

---
+++ src/library/boostedNNClass.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Christopher Chang
+"""
+
+class boostedNNClass:
+    def __init__(self, superModel, subModels, inputDim, outputNum):
+        #Models and model info
+        self.superModel = superModel
+        self.subModels = subModels
+        self.inputDim = inputDim
+        self.outputNum = outputNum
+        
+    def trainSubClass(xTrain, yTrain, subModelNum):
+        pass
+        
+    
+        
