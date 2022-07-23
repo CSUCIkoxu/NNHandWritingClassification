f34c020190273dd557423c90130f800360901a08
CSUCIkoxu <70290605+CSUCIkoxu@users.noreply.github.com>
2022-07-22 18:45:18 -0700

    Implemented the driver code for boostedCNN
    Implemented trainModel, createSuperModel, and createModel to drive the code in boostedCNN
    
    Needs testing

--- src/boostedCNN.py
+++ src/boostedCNN.py
@@ -9,10 +9,11 @@ to read hand written letters
 """
 import numpy as np
 import pandas as pd
-import library.imageProcessor as imgProcessor
 import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import layers
+import library.imageProcessor as imgProcessor
+import library.boostedNNClass as boostedNNClass
 
 def categorizeLabels(y, numClasses):
     return tf.keras.utils.to_categorical(y, num_classes=numClasses, dtype = "int32")
@@ -40,83 +41,61 @@ def createSubModel(inputShape):
     
     return model
 
-def createModel(inputShape, outputNum):
-    
+def createSuperModel(inputShape, outputNum):
     model = None
     
     cnn = keras.Sequential()
-    cnn.add(layers.Conv2D(62, 5, (3,3), input_shape=inputShape, activation='relu', padding='valid'))  #(filters, kernel size, stride)
-    cnn.add(keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(1,1), padding="valid", data_format=None))
-    cnn.add(layers.Conv2D(124, 3, (1,1), activation='relu', padding="valid"))
-    cnn.add(keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2,2), padding="valid", data_format=None))
-    cnn.add(layers.Conv2D(248, 3, (1,1), activation='relu', padding="valid"))
-    cnn.add(keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2,2), padding="valid", data_format=None))
-    cnn.add(layers.Flatten())
-    cnn.add(layers.Dense(512, activation='relu'))
+    cnn.add(layers.Dense(62, activation='relu', input_shape=inputShape))
+    cnn.add(layers.Dense(124, activation='relu'))
     cnn.add(layers.Dropout(0.3))
-    cnn.add(layers.Dense(256, activation='relu'))
+    cnn.add(layers.Dense(62, activation='relu'))
     cnn.add(layers.Dense(outputNum, activation='softmax'))     #output
     
     model = cnn
     
     return model
 
-def trainModel(model, imageProc, categoryNum, xTrain, yTrain):
-    trainedModel = None
-    hyperParameters = []
-    trainingScore = 0
+def createModel(inputShape, outputNum):
+    model = None
     
-    yTrainVect = categorizeLabels(yTrain, categoryNum)
+    subModels = []
+    superModel = None
     
-    xTrainNew, yTrainNew, xValid, yValid = imageProc.splitData(xTrain, yTrainVect, 123)
+    for i in range(outputNum):
+        subModels.append(createSubModel(inputShape))
     
-    #Callback for early stopping
-    callbackBest = keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+    superModel = createSuperModel(outputNum, outputNum)
     
-    #Hold-out Cross-validation
-    scores = {}
-    scoreEpoch = {}
-    optimizers = ['adadelta', 'adagrad', 'adam', 'adamax', 'ftrl', 'nadam', 'rmsprop', 'sgd']
-    lossFuncs = ['categorical_crossentropy', 'kl_divergence']#, 'poisson']  #poisson gives nan values for some reason
-    metrics = [['categorical_crossentropy']]
+    model = boostedNNClass.boostedNNClass(superModel, subModels, inputShape, outputNum)
     
-    for o in optimizers:
-        for l in lossFuncs:
-            for m in metrics:
-                #Debug Logger
-                print("{}, {}, {}".format(o, l, m))
-                
-                currModel = keras.models.clone_model(model)
-                currModel.compile(optimizer=o, loss=l, metrics=m)
-                
-                modelHist = currModel.fit(xTrainNew, yTrainNew, epochs=50, batch_size=20, validation_data=(xValid,yValid), callbacks=[callbackBest])
-                
-                #Since we have early stopping, we want to get the epoch at which we had the best score
-                minScore = min(modelHist.history["val_" + m[0]])
-                for i in range(len(modelHist.history["val_" + m[0]])):
-                    if (modelHist.history["val_" + m[0]][i] == minScore):
-                            scoreEpoch[(o, l, m[0])] = i + 1
-                            break
-                            
-                scores[(o, l, m[0])] = minScore
-                
-    trainingScore = min(scores.values())
-    for k in scores.keys():
-        if (trainingScore == scores[k]):
-            hyperParameters = k
-            break
+    return model
     
-    #Recreate the best model
-    trainedModel = keras.models.clone_model(model)
-    trainedModel.compile(optimizer=hyperParameters[0], 
-               loss=hyperParameters[1], 
-               metrics=[hyperParameters[2]])
+
+def trainModel(model, imageProc, trainingQuantity, categoryNum):
+    # trainedModel = None
+    subHyperParameters = []
+    superHyperParameters = []
+    subTrainingScore = []
+    superTrainingScore = 0
     
-    #Train the best model on the whole data
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir="../logs/boostedCNN", histogram_freq=1)
-    trainedModel.fit(xTrain, yTrainVect, epochs=scoreEpoch[hyperParameters], batch_size=20, callbacks=[tensorboard_callback])
+    #Train the Sub-Models
+    for c in imageProc.characterList:
+        ##Get training data for sub model
+        # xTrain, yTrain = imageProc.getTrainingData(c, 123)
+        xTrainNew, yTrainNew, xValid, yValid = imageProc.splitData(*imageProc.getTrainingData(c, 123), seed=123)
+        
+        ##Train one sub-model
+        model.trainSubModel(xTrainNew, yTrainNew, imageProc.char2Enum(c), xValid, yValid)
+        
+    #Train the Super-Model
+    ##Get the training data for the super model
+    # xTrain, yTrain = imageProc.getRandomTrainingData(trainingQuantity, 123)
+    xTrainNew, yTrainNew, xValid, yValid = imageProc.splitData(*imageProc.getRandomTrainingData(trainingQuantity, 123), seed=123)
     
-    return trainedModel, hyperParameters, trainingScore
+    ##Train the super-model
+    model.trainSuperModel(xTrainNew, yTrainNew, categoryNum, xValid, yValid)
+    
+    return subHyperParameters, superHyperParameters, subTrainingScore, superTrainingScore
 
 def testModel(model, imageProc, xTest, yTest):
     yPred = []
@@ -140,15 +119,14 @@ def testModel(model, imageProc, xTest, yTest):
 #import the image processor class with all the helper functions
 imageProc = imgProcessor.imageProcessor()
 
-#Get data
-xTrain, yTrain = imageProc.getRandomTrainingData(200, seed=123)
-xTest, yTest = imageProc.getRandomTestingData(10, seed=123)
-
 #Create the model
 model = createModel((128,128,1), 62)
 
 #Train the model, get hyperParameters, and get the training score
-trainedModel, hyperParams, trainingScore = trainModel(model, imageProc, len(imageProc.characterList), xTrain, yTrain)
+trainedModel, hyperParams, trainingScore = trainModel(model, imageProc, len(imageProc.characterList))
+
+#Get data
+xTest, yTest = imageProc.getRandomTestingData(50, seed=123)
 
 #Test the model and calculate stats
 yPred, f1Score, confMat, classReport = testModel(trainedModel, imageProc, xTest, yTest)
@@ -157,7 +135,7 @@ print(classReport)
 print(confMat)
 
 #Save the model
-trainedModel.save(imageProc.modelSavesDirectory + '/boostedCNN')
+# trainedModel.save(imageProc.modelSavesDirectory + '/boostedCNN')
 
 
 #Use this to generate tensorboard charts on saved models
